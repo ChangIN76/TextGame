dbfce39d31cf40ee67400871cf6e4f529c6bf814
ChangIN76 <vel5535@naver.com>
2024-09-11 21:46:29 +0900

    (2024.9.11) 좌표 이동 함수

--- Program/Source.c
+++ Program/Source.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <windows.h>
+
+#define SIZE 10
 
 void shuffle(int array[], int size)
 {
@@ -19,6 +22,7 @@ void shuffle(int array[], int size)
 
 int main()
 {
+
 #pragma region 셔플 함수
 
 	//  srand(time(NULL));
@@ -39,22 +43,57 @@ int main()
 #pragma endregion
 
 #pragma region 포인터 배열
+		
+	//  const char* dialog[10];
+	//  
+	//  dialog[0] = "안녕하세요?";
+	//  dialog[1] = "누구신가요?";
+	//  dialog[2] = "탐정입니다.";
+	//  dialog[3] = "저는 의뢰인입니다.";
+	//  dialog[4] = "수상한 사건이 있다고 해서 왔습니다.";
+	//  dialog[5] = "네 맞습니다.";
+	//  dialog[6] = "이 사건에 대해 아시는 게 있나요?";
+	//  dialog[7] = "조금 알고 있습니다";
+	//  dialog[8] = "말씀해 주세요";
+	//  dialog[9] = "흠 ..";
+	//  
+	//  int i = 0;
+	//  
+	//  while (i < SIZE)
+	//  {
+	//  	// 0X0000 : 이전에 누른 적이 없고 호출 시점에도 눌려있지 않은 상태
+	//  
+	//  	// 0X0001 : 이전에 누른 적이 있고 호출 시점에는 눌려있지 않은 상태
+	//  
+	//  	// 0X8000 : 이전에 누른 적이 없고 호출 시점에는 눌려있는 상태
+	//  
+	//  	// 0X8001 : 이전에 누른 적이 있고 호출 시점에도 눌려있는 상태
+	//  
+	//  
+	//  	if (GetAsyncKeyState(VK_SPACE) & 0X0001)
+	//  	{
+	//  		if (i % 2 == 0)
+	//  		{
+	//  			printf("탐정 : %s\n", dialog[i]);
+	//  		}
+	//  		else
+	//  		{
+	//  			printf("의뢰인 : %s\n", dialog[i]);
+	//  		}
+	//  		i++;
+	//  	}
+	//  	
+	//  }
+	//  
+	//  
+	//  printf("대화 종료");
 
-	const char * dialog[3];
-
-	dialog[0] = "안녕하세요?";
-	dialog[1] = "누구신가요?";
-	dialog[2] = "탐정입니다.";
-
-	for (int i = 0; i < 3; i++)
-	{
-		printf("%s\n", dialog[i]);
-
-	}
-	
 
 #pragma endregion
 
-
 	return 0;
 }
+
+
+
+
